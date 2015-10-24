7efeadf75b8c8239d1a6c3e7a609d90bd56b464c
kylegearhart <hkgearhart@gmail.com>
2015-10-24 22:29:03 +0900

    Added constants

---
+++ SigFigCalculator/SFConstants.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+// In-App Purchases
+static NSString *const kRemoveAdsProductIdentifier = @"com.kylegearhart.sigfigcalculator.removeadvertisements";
+static NSString *const kAdsRemovedUserDefaultBoolean = @"adsRemoved";
+
+// iAd
+static NSString *const kBannerViewAdvertisementReceived = @"BannerViewActionWillBegin";
+static NSString *const kBannerViewFinishedDisplay = @"BannerViewActionDidFinish";
