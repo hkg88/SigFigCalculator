827f8bbcbd34133db533b073bd0eb605118c7b59
kylegearhart <hkgearhart@gmail.com>
2015-12-05 16:56:35 +0900

    Use a decimal keyboard for counter screen / Added toolbar

--- SigFigCalculator/SFCounterViewController.m
+++ SigFigCalculator/SFCounterViewController.m
@@ -10,11 +10,9 @@
 @property (strong, nonatomic) IBOutlet UILabel *numSigFigsLabel;
 @property (strong, nonatomic) IBOutlet UILabel *numberTextLabel;
 @property (strong, nonatomic) IBOutlet UILabel *numSigFigsTextLabel;
+@property (strong, nonatomic) NSString *valueBeforeEdit;
 
 @property (strong, nonatomic) SigFigCounter *sigFigCounter;
-
-- (IBAction)backgroundTapped:(UIControl *)sender;
-- (IBAction)numberEntered:(UITextField *)sender;
 
 @end
 
@@ -34,21 +32,68 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredContentSizeChanged:) name:UIContentSizeCategoryDidChangeNotification object:nil];
     
     self.keyboardDismissalTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
-                                                                                         action:@selector(dismissKeyboard:)];
+                                                                                         action:@selector(numberEntered)];
     self.keyboardDismissalTapGestureRecognizer.cancelsTouchesInView = NO;
     [self.view addGestureRecognizer:self.keyboardDismissalTapGestureRecognizer];
+    
+    [self setupKeyboardAccessoryToolbar];
 }
 
-- (void)dismissKeyboard:(UIGestureRecognizer *)gestureRecognizer
+- (void)setupKeyboardAccessoryToolbar
+{
+    UIToolbar *numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 50)];
+    numberToolbar.barStyle = UIBarStyleDefault;
+    numberToolbar.items = @[[[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain
+                                                            target:self
+                                                            action:@selector(cancelPressed)],
+                            [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                                         target:nil
+                                                                         action:nil],
+                            [[UIBarButtonItem alloc]initWithTitle:@"+/-"
+                                                            style:UIBarButtonItemStylePlain
+                                                           target:self
+                                                           action:@selector(changeSignPressed)],
+                            [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                                         target:nil
+                                                                         action:nil],
+                            [[UIBarButtonItem alloc]initWithTitle:@"Done"
+                                                            style:UIBarButtonItemStyleDone
+                                                           target:self
+                                                           action:@selector(numberEntered)]];
+    [numberToolbar sizeToFit];
+    self.textField.inputAccessoryView = numberToolbar;
+}
+
+- (void)changeSignPressed
+{
+    if ([self.textField.text isEqualToString:@""]) {
+        return;
+    }
+    
+    NSMutableString *mutableText = self.textField.text.mutableCopy;
+    if ([self.textField.text hasPrefix:@"-"]) {
+        self.textField.text = [self.textField.text.mutableCopy substringWithRange:NSMakeRange(1, mutableText.length - 1)];
+    } else {
+        self.textField.text = [@"-".mutableCopy stringByAppendingString:self.textField.text];
+    }
+}
+
+-(void)cancelPressed
+{
+    self.textField.text = self.valueBeforeEdit;
+    [self dismissKeyboard];
+}
+
+- (void)dismissKeyboard
 {
     [self.view endEditing:YES];
 }
 
-- (IBAction)numberEntered:(UITextField *)sender
+- (void)numberEntered
 {
     // Only attempt and count the SigFigs of a non-empty NSString
     if(![self.textField.text isEqualToString:@""]) {
-        int numSigFigs = [self.sigFigCounter countSigFigs:sender.text];
+        int numSigFigs = [self.sigFigCounter countSigFigs:self.textField.text];
         // The SigFigCounter will return -1 if the inputted number isn't valid
         if(numSigFigs >= 0) {
             self.numSigFigsLabel.text = [NSString stringWithFormat:@"%d", numSigFigs];
@@ -58,21 +103,8 @@
     } else {
         self.numSigFigsLabel.text = @"";
     }
-}
-
-#pragma mark UITextFieldDelegate
-
-- (void)backgroundTapped:(UIControl *)sender
-{
-    [self.textField resignFirstResponder];
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField
-{
-    if (textField == self.textField) {
-        [textField resignFirstResponder];
-    }
-    return NO;
+    
+    [self dismissKeyboard];
 }
 
 - (void)preferredContentSizeChanged:(NSNotification *)notification
@@ -80,6 +112,14 @@
     self.numberTextLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
     self.textField.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
     self.numSigFigsTextLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+#pragma mark - UITextViewDelegate
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    // Store the current value prior to editing to be used if the user cancels their edit
+    self.valueBeforeEdit = self.textField.text;
 }
 
 @end
