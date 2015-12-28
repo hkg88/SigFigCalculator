007f3b3997281d9ef934bc3a4e669fa9d2ed3c1c
kylegearhart <hkgearhart@gmail.com>
2015-12-28 16:08:53 -0600

    Put extern defined constants at top of .h file

--- SigFigCalculator/SFBannerViewController.h
+++ SigFigCalculator/SFBannerViewController.h
@@ -1,9 +1,9 @@
+extern NSString *const BannerViewActionWillBegin;
+extern NSString *const BannerViewActionDidFinish;
+
 @interface SFBannerViewController : UIViewController
 
 - (void)showBannerView;
 - (void)hideBannerView;
 
 @end
-
-extern NSString *const BannerViewActionWillBegin;
-extern NSString *const BannerViewActionDidFinish;
